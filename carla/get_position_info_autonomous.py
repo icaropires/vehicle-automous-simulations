ad7641fb9609aaec66965c2349ca1cb8f876ba7f
Ícaro Pires <icaropsa@gmail.com>
2020-02-01 21:24:24 -0300

    Add imu sensor and make some adjustments to get_position_info_autonomous

--- carla/get_position_info_autonomous.py
+++ carla/get_position_info_autonomous.py
@@ -27,8 +27,13 @@ CONTROL_PARAMS = ('throttle', 'steer', 'brake', 'hand_brake',
 
 GNSS_PARAMS = ('latitude', 'longitude', 'altitude')
 
+IMU_PARAMS = ('compass',)
+IMU_COORDINATE_PARAMS = ('accelerometer', 'gyroscope')
 
-if __name__ == '__main__':
+POS_TICK_INTERVAL = str(1/50)  # seconds
+
+
+def main():
     ticks = []
     actor_list = []
 
@@ -38,6 +43,7 @@ if __name__ == '__main__':
 
     pos_file = open(f'{out_folder}/pos.csv', 'w')
     gnss_file = open(f'{out_folder}/gnss.csv', 'w')
+    imu_file = open(f'{out_folder}/imu.csv', 'w')
 
     client = carla.Client('localhost', 2000)
     client.set_timeout(2.0)
@@ -45,9 +51,9 @@ if __name__ == '__main__':
     world = client.get_world()
     blueprint_library = world.get_blueprint_library()
 
-
-    def print_pos_labels():
+    def write_pos_labels():
         labels = ['timestamp']
+
         labels.extend(f'{attr}_{c}' for attr in COORDINATES_PARAMS
                       for c in ('x', 'y', 'z'))
 
@@ -55,49 +61,77 @@ if __name__ == '__main__':
 
         labels.extend(f'control_{attr}' for attr in CONTROL_PARAMS)
 
-        # print(*labels, sep=',')
-        pos_file.write(','.join(map(str, labels)) + '\n')
+        pos_file.write(','.join(labels) + '\n')
 
-    def print_pos_info(w_snapshot):
+    def write_pos_values(w_snapshot):
         coordinates_attrs = (
             getattr(vehicle, 'get_' + p)() for p in COORDINATES_PARAMS
         )
 
+        # Get timestamp value
         values = [w_snapshot.platform_timestamp]
+
+        # Get COORDINATES_PARAMS values
         values.extend(
             getattr(attr, c) for attr in coordinates_attrs
             for c in ('x', 'y', 'z')
         )
 
+        # Get ROTATION_PARAMS values
         rotation = vehicle.get_transform().rotation
         values.extend(
             getattr(rotation, attr) for attr in ROTATION_PARAMS
         )
 
+        # Get CONTROL_PARAMS values
         control = vehicle.get_control()
         values.extend(
             getattr(control, attr) for attr in CONTROL_PARAMS
         )
 
-        # print(*values, sep=',')
         pos_file.write(','.join(map(str, values)) + '\n')
 
-    def print_gnss_labels():
-        labels = ['timestamp']
-        labels.extend(attr for attr in GNSS_PARAMS)
+    def write_gnss_labels():
+        gnss_file.write(','.join(('timestamp',) + GNSS_PARAMS) + '\n')
 
-        # print(*labels, sep=',')
-        gnss_file.write(','.join(map(str, labels)) + '\n')
+    def write_imu_labels():
+        coordinates_attrs = tuple(
+            f'{attr}_{c}' for attr in IMU_COORDINATE_PARAMS
+            for c in ('x', 'y', 'z')
+        )
 
-    def print_gnss_info(data):
-        values = [world.get_snapshot().platform_timestamp]
+        imu_file.write(','.join(
+            ('timestamp',) + coordinates_attrs + IMU_PARAMS
+        ) + '\n')
+
+    def write_gnss_values(data):
+        snapshot = world.get_snapshot()
+        write_pos_values(snapshot)  # Small gambiarra
+
+        values = [snapshot.platform_timestamp]
 
         values.extend(
             getattr(data, attr) for attr in GNSS_PARAMS
         )
 
-        # print(*values, sep=',')
         gnss_file.write(','.join(map(str, values)) + '\n')
+
+    def write_imu_values(data):
+        values = [world.get_snapshot().platform_timestamp]
+
+        values.extend(
+            getattr(data, attr) for attr in IMU_PARAMS
+        )
+
+        attrs = (
+            getattr(data, attr) for attr in IMU_COORDINATE_PARAMS
+        )
+        values.extend(
+            getattr(attr, c) for attr in
+            attrs for c in ('x', 'y', 'z')
+        )
+
+        imu_file.write(','.join(map(str, values)) + '\n')
 
     def get_autonomous():
         vehicle_bp = random.choice(
@@ -108,14 +142,34 @@ if __name__ == '__main__':
             world.get_map().get_spawn_points()
         )
         vehicle = world.spawn_actor(vehicle_bp, transform)
+
+        # Carla in 0.9.7 is throwing SIGABRT after ending when using autopilot
         vehicle.set_autopilot()
 
         actor_list.append(vehicle)
         return vehicle
 
+    def get_camera(vehicle):
+        camera_bp = blueprint_library.find('sensor.camera.rgb')
+        camera_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
+        camera_bp.set_attribute('image_size_x', '800')
+        camera_bp.set_attribute('image_size_y', '600')
+        camera_bp.set_attribute('sensor_tick', '0.3')
+
+        camera = world.spawn_actor(
+            camera_bp,
+            camera_transform,
+            attach_to=vehicle
+        )
+        actor_list.append(camera)
+
+        return camera
+
     def get_gnss(vehicle):
+        gnss_transform = carla.Transform(carla.Location(x=0.5, z=0.5))
         gnss_bp = blueprint_library.find('sensor.other.gnss')
-        gnss_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
+        gnss_bp.set_attribute('sensor_tick', POS_TICK_INTERVAL)
+
         gnss = world.spawn_actor(
             gnss_bp,
             gnss_transform,
@@ -125,28 +179,22 @@ if __name__ == '__main__':
 
         return gnss
 
-    def get_camera(vehicle):
-        camera_bp = blueprint_library.find('sensor.camera.rgb')
-        camera_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
-        camera_bp.set_attribute('image_size_x', '800')
-        camera_bp.set_attribute('image_size_y', '600')
-        camera_bp.set_attribute('sensor_tick', '0.3')
-        camera = world.spawn_actor(
-            camera_bp,
-            camera_transform,
+    def get_imu(vehicle):
+        imu_transform = carla.Transform(carla.Location(x=0.5, z=0.5))
+        imu_bp = blueprint_library.find('sensor.other.imu')
+        imu_bp.set_attribute('sensor_tick', POS_TICK_INTERVAL)
+
+        imu = world.spawn_actor(
+            imu_bp,
+            imu_transform,
             attach_to=vehicle
         )
-        actor_list.append(camera)
-        
-        return camera
+        actor_list.append(imu)
+
+        return imu
 
     try:
         vehicle = get_autonomous()
-
-        print('Initiating writting of gnss data...')
-        gnss = get_gnss(vehicle)
-        print_gnss_labels()
-        gnss.listen(print_gnss_info)
         spectator = world.get_spectator()
 
         def follow(_):
@@ -154,22 +202,33 @@ if __name__ == '__main__':
             t.x -= 5
             t.z += 5
             spectator.set_location(t)
-
         ticks.append(world.on_tick(follow))
 
-        print('Initiating getting of position data...')
-        print_pos_labels()
-        ticks.append(
-            world.on_tick(print_pos_info)
-        )
+        print('Initiating writing of position data...')
+        write_pos_labels()
+        # Write pos labels inside gnss handle function
 
-        print('Initiating getting of position data...')
+        # ticks.append(
+        #     world.on_tick(write_pos_values)
+        # )
+
+        print('Initiating writing of gnss data...')
+        gnss = get_gnss(vehicle)
+        write_gnss_labels()
+        gnss.listen(write_gnss_values)
+
+        print('Initiating writing of imu data...')
+        imu = get_imu(vehicle)
+        write_imu_labels()
+        imu.listen(write_imu_values)
+
+        print('Initiating camera recording...')
         camera = get_camera(vehicle)
         camera.listen(lambda image: image.save_to_disk(
             f'{out_folder}/{image.frame:06d}.png'
         ))
 
-        duration = 60
+        duration = 60*5
         print()
         for i in range(duration, 0, -1):
             print(f'Letting car drive for more {i}s...', end='\r')
@@ -184,10 +243,24 @@ if __name__ == '__main__':
 
         print('Destroying actors...')
         for actor in actor_list:
+            if isinstance(actor, carla.libcarla.Vehicle):  # Avoid segfault
+                actor.set_autopilot(False)
+                continue  # Let vehicles for later (segfault)
+
             actor.destroy()
 
         time.sleep(0.5)
+
+        # Destroy vehicles
+        for actor in actor_list:
+            actor.destroy()
+
         pos_file.close()
         gnss_file.close()
+        imu_file.close()
 
         print('done.')
+
+
+if __name__ == '__main__':
+    main()
