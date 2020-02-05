45e9a3442e66980b9839d32d917c4283b0682128
Ícaro Pires <icaropsa@gmail.com>
2020-02-05 20:48:07 -0300

    Get data from datafiles

--- canvas.py
+++ canvas.py
@@ -1,3 +1,5 @@
+import sys
+import csv
 import tkinter
 
 
@@ -12,9 +14,16 @@ class GUI:
     transponder_radius = 20
     transponder_color = "red"
 
+    default_filename = "data.csv"
+
+    csv_window_w = 1800
+    csv_window_h = 1800
+    scale_from_csv = window_h / csv_window_h  # Assuming width = height
+
+    measurements_interval = 200
+
     def __init__(self):
         self.window = tkinter.Tk()
-
         self.window.title("GUI")
 
         self.canvas = tkinter.Canvas(
@@ -26,29 +35,40 @@ class GUI:
 
         self.canvas.pack()
 
+        self.datafile = self.read_datafile()
+
+        self._progress_bar = None
+
     def run(self):
         self.canvas.after(300, self._handle_update)
         self.window.mainloop()
 
     def _move_transponder(self):
         r = self.transponder_radius
-        x, y, *_ = self.canvas.coords(self.transponder)
 
-        x += 1
-        y = (x / 20) ** 2
+        try:
+            x, y = next(self.datafile)
+        except StopIteration:
+            x, y = (-self.window_w, -self.window_h)
 
-        coords = (x, y, x + 2 * r, y + 2 * r)
+        coords = (x - r, y - r, x + r, y + r)
         self.canvas.coords(self.transponder, coords)
+
+        print("New coords:", x, y)
 
     def _handle_update(self):
         self._move_transponder()
-        self.canvas.after(10, self._handle_update)
+        self.canvas.after(self.measurements_interval, self._handle_update)
 
     def add_transponder(self):
         r = self.transponder_radius
+        ww = self.window_w
+        wh = self.window_h
+
+        x, y = -ww, -wh
 
         return self.canvas.create_oval(
-            0, 0, 2 * r, 2 * r, fill=self.transponder_color
+            x, y, x + 2 * r, y + 2 * r, fill=self.transponder_color
         )
 
     def add_activators(self):
@@ -67,6 +87,29 @@ class GUI:
         )
 
         return [self.canvas.create_oval(c, fill=fill) for c in coords]
+
+    def read_datafile(self):
+        filename = self.default_filename
+
+        if len(sys.argv) > 1:
+            filename = sys.argv[1]
+
+        with open(filename, "r") as f:
+            rows = csv.DictReader(f)
+
+            content = [
+                (int(r["estimated_x"]), int(r["estimated_y"]))
+                for r in rows
+                if int(r["id_activator"]) == 1
+            ]
+
+        # To generator and correct scale
+        content = (
+            (x * self.scale_from_csv, y * self.scale_from_csv)
+            for x, y in content
+        )
+
+        return content
 
 
 gui = GUI()
