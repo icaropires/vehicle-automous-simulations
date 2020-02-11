e103b5a9434f0601e50c90a36c3ab9ee47561539
Ícaro Pires <icaropsa@gmail.com>
2020-02-11 11:14:05 -0300

    Add trilateration algorithm and basic README

--- plot.py
+++ plot.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-filename = 'data.csv'
+filename = "data.csv"
 if len(sys.argv) > 1:
     filename = sys.argv[1]
 
---
+++ trilateration.py
@@ -0,0 +1,17 @@
+# https://www.101computing.net/cell-phone-trilateration-algorithm/
+def trilaterate(coords1, coords2, coords3):
+    x1, y1, r1 = coords1
+    x2, y2, r2 = coords2
+    x3, y3, r3 = coords3
+
+    A = 2 * x2 - 2 * x1
+    B = 2 * y2 - 2 * y1
+    C = r1 ** 2 - r2 ** 2 - x1 ** 2 + x2 ** 2 - y1 ** 2 + y2 ** 2
+    D = 2 * x3 - 2 * x2
+    E = 2 * y3 - 2 * y2
+    F = r2 ** 2 - r3 ** 2 - x2 ** 2 + x3 ** 2 - y2 ** 2 + y3 ** 2
+
+    x = (C * E - F * B) / (E * A - B * D)
+    y = (C * D - A * F) / (B * D - A * E)
+
+    return x, y
